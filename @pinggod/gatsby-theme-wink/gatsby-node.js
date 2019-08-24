a7582f05b45c4772808e9d7b74aac728502b141b
pinggod <pinggodstudio@gmail.com>
2019-08-24 23:12:31 +0800

    dep: add registry address

--- @pinggod/gatsby-theme-wink/gatsby-node.js
+++ @pinggod/gatsby-theme-wink/gatsby-node.js
@@ -1,11 +1,64 @@
-require("source-map-support").install();
-require("ts-node").register({
-    files: true,
-    compilerOptions: {
-        module: "commonjs",
-        target: "es2017"
-    }
-});
+// eslint-disable-next-line
+const { createFilePath } = require("gatsby-source-filesystem");
 
-exports.onCreateNode = require("./gatsby/node/onCreateNode").onCreateNode;
-exports.createPages = require("./gatsby/node/createPages").createPages;
+exports.onCreateNode = ({ node, actions, getNode }) => {
+    const { createNodeField } = actions;
+
+    if (node.internal.type === `Mdx`) {
+        createNodeField({
+            node,
+            name: `slug`,
+            value: createFilePath({ node, getNode })
+        });
+    }
+};
+
+// import { gql } from 'apollo-boost'
+
+// export default gql`
+//   query Pokemons($first: Int!) {
+//     pokemons(first: $first) {
+//       id
+//       number
+//       name
+//     }
+//   }
+// `
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { errors, data } = await graphql(`
+        {
+            allMdx {
+                totalCount
+                posts: edges {
+                    node {
+                        id
+                        body
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
+    if (errors) {
+        throw errors;
+    }
+
+    const { createPage } = actions;
+    const { posts } = data.allMdx;
+    const postTemplate = require.resolve("./src/templates/post.tsx");
+
+    posts.forEach(post => {
+        createPage({
+            path: post.node.fields.slug,
+            component: postTemplate,
+            context: {
+                id: post.node.id,
+                post: post
+            }
+        });
+    });
+};
